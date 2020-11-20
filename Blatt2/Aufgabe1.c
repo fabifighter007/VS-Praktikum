0f547727401d9d8a630e8ac664ea8d4b25ce711b
Fabian Schilling <your.email@your.provider>
2020-11-20 13:38:19 +0100

    Blatt2/1 fast fertig

--- Blatt2/Aufgabe1.c
+++ Blatt2/Aufgabe1.c
@@ -14,31 +14,71 @@ int nextGabel(int);
 
 typedef enum status {DENKEN, ESSEN} STATUS;
 
+
 typedef struct person {
   int id;
   enum status status;
-} person;
+} person_t;
 
+person_t person[5];
+
+void neuerStatus(int i) {
+  if(person[i].status == 0) {
+    person[i].status = 1;
+  } else {
+    person[i].status = 0;
+  }
+}
+
+void palast() {
+  while(1) {
+    time_t t;
+//    srand((unsinged) time(t));
+    
+    int r = rand()%5;
+    if(person[r].status == DENKEN) {
+      printf("Person %d möchte essen", r);
+      person[r].status = ESSEN;
+    } else {
+      printf("Person %d möchte DENKEN", r);
+      person[r].status = DENKEN;
+    }
+
+/*    sperreGabel(r);
+    neuerStatus(r);
+    sleep(1);
+    V(r);
+  */
+    }
+}
 
 void P(int sem_num) {
-  struct sembuf semaphore;
-  semaphore.sem_num = sem_num;
-  semaphore.sem_op = -1; // P -> -1
-  semaphore.sem_flg = ~(IPC_NOWAIT|SEM_UNDO);
+  struct sembuf semaphore[2];
+  semaphore[0].sem_num = sem_num;
+  semaphore[0].sem_op = -1; // P -> -1
+  semaphore[0].sem_flg = ~(IPC_NOWAIT|SEM_UNDO);
 
-  if(semop(sem_id, &semaphore, 1)) {
+  semaphore[1].sem_num = nextGabel(sem_num);
+  semaphore[1].sem_op = -1; // P -> -1
+  semaphore[1].sem_flg = ~(IPC_NOWAIT|SEM_UNDO);
+
+  if(semop(sem_id, semaphore, 2)) {
     perror("Fehler bei P() \n");
     exit(1);
   }
 }
 
 void V(int sem_num) {
-  struct sembuf semaphore;
-  semaphore.sem_num = sem_num;
-  semaphore.sem_op = 1; // V -> 1
-  semaphore.sem_flg = ~(IPC_NOWAIT|SEM_UNDO);
+  struct sembuf semaphore[2];
+  semaphore[0].sem_num = sem_num;
+  semaphore[0].sem_op = 1; // V -> 1
+  semaphore[0].sem_flg = ~(IPC_NOWAIT|SEM_UNDO);
 
-  if(semop(sem_id, &semaphore, 1)) {
+  semaphore[1].sem_num = nextGabel(sem_num);
+  semaphore[1].sem_op = 1; // V -> 1
+  semaphore[1].sem_flg = ~(IPC_NOWAIT|SEM_UNDO);
+  
+  if(semop(sem_id, semaphore, 2)) {
     perror("Fehler bei V() \n");
     exit(1);
   }
@@ -50,8 +90,10 @@ void sperreGabel(int id) {
   P(nextGabel(id));
 }
 
-void freigebenGabel() {
-  printf();
+void freigebenGabel(int id) {
+  printf("Gebe Gabel frei %d \n", id);
+  V(id);
+  P(nextGabel(id));
 }
 
 int nextGabel(int id) {
@@ -79,10 +121,11 @@ int main(){
   
   init_sem();
 
-  person person[5];
   for(int i=0;i<5;i++) {
     person[i].id = i;
     person[i].status = DENKEN;
   }
   sperreGabel(2);
+  freigebenGabel(2);
+  palast();
 }
